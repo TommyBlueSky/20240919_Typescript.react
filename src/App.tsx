8b10453a4de2d9aec773f3f6e2b0302a477b7807
TommyBlueSky <t07y31@gmail.com>
2024-09-19 02:05:07 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default App;
